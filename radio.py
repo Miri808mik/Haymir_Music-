36224c238ce4452a2ab78e6cd5e6ef74f486487d
Miri808mik <mohamad139043210@gmail.com>
2026-08-14 15:06:24 +0330

    Update radio.py

--- radio.py
+++ radio.py
@@ -55,4 +55,4 @@ def home():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5005))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, threaded=True)
